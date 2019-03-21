55f181d4cf728ce41b7cc74018fa01b872b4a0c9
Boboaca Costel <bobyca2003@yahoo.com>
2019-03-21 09:41:49 +0100

    Add files via upload

---
+++ SerialEvent/SerialEvent.ino
@@ -0,0 +1,67 @@
+/*
+  Serial Event example
+
+  When new serial data arrives, this sketch adds it to a String.
+  When a newline is received, the loop prints the string and clears it.
+
+  A good test for this is to try it with a GPS receiver that sends out
+  NMEA 0183 sentences.
+
+  NOTE: The serialEvent() feature is not available on the Leonardo, Micro, or
+  other ATmega32U4 based boards.
+
+  created 9 May 2011
+  by Tom Igoe
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/SerialEvent
+*/
+
+String inputString = "";         // a String to hold incoming data
+bool stringComplete = false;  // whether the string is complete
+int red=1000;
+int green=1000;
+int blue =100;
+
+void setup() {
+  // initialize serial:
+  Serial.begin(9600);
+  // reserve 200 bytes for the inputString:
+  inputString.reserve(200);
+}
+
+void loop() {
+  // print the string when a newline arrives:
+  if (stringComplete) {
+    Serial.print(red);
+    Serial.print(green);
+    Serial.print(blue);
+    // clear the string:
+    inputString = "";
+    stringComplete = false;
+  }
+   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(red);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(green);  
+  
+}
+
+/*
+  SerialEvent occurs whenever a new data comes in the hardware serial RX. This
+  routine is run between each time loop() runs, so using delay inside loop can
+  delay response. Multiple bytes of data may be available.
+*/
+void serialEvent() {
+  while (Serial.available()) {
+       red = Serial.parseInt();
+    // do it again:
+    green = Serial.parseInt();
+    // do it again:
+    blue = Serial.parseInt();
+    if (Serial.read() == '\n') {
+      stringComplete = true;
+    }
+  }
+}
